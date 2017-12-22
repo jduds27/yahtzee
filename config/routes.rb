e52331f0af0915353fc6f21e8bf9c7e1e2fc7431
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-22 14:29:39 -0600

    Started on game controller and application navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/leaderboard'
+  get 'leaderboard', to: 'pages#leaderboard'
 
-  get 'pages/how_to_play'
+  get 'how_to_play', to: 'pages#how_to_play'
+
+
+  root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
